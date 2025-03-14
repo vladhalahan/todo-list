9fbdc18432540efc2b5d762832b951a5d547815b
Vlad <vhalahan@simulmedia.com>
2025-03-14 14:00:20 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const Database = require("better-sqlite3");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ 
+};
+
+module.exports = {
+  output: "standalone",
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["node_modules/better-sqlite3"]
+    }
+  }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Provider from "@/app/_trpc/Provider";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,15 +13,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
